6f0deb4346e48ae7a433cafd69ed547bfffa595e
joodh999 <joodhm95@gmail.com>
2020-12-17 17:52:15 +0500

    :-/

--- Gameoflife.go
+++ Gameoflife.go
@@ -6,20 +6,75 @@ import (
 	"time"
 )
 
-var gridstate [20][20]int
+var grid [20][20]int
 var gen = 0
 
 func main() {
 
-	state := flag.Int("state", 0, "set state (0-3)")
-	gen := flag.Int("gen", 10, "set generation")
+	state := flag.Int("S", 0, "set state (0-3)")
+	gens := flag.Int("G", 10, "set generation")
+	flag.Parse()
 
-	print(*state)
-	println(*gen)
-	// setlife(3)
+	println(*state)
+	println(*gens)
+
+	grid[9][9] = 1
+
+	grid[8][9] = 1
+	grid[10][9] = 1
+
+	grid[10][10] = 1
+	grid[10][8] = 1
+
+	// display()
+	nextgen()
 	// display()
 
 }
+
+func nextgen() {
+	for i := 0; i < 20; i++ {
+		for j := 0; j < 20; j++ {
+			if grid[i][j] == 1 {
+
+				// Each cell with one or no neighbors dies, as if by solitude.
+				if getneighbors(i, j) == 0 || getneighbors(i, j) == 1 {
+					grid[i][j] = 0
+				}
+
+				// Each cell with four or more neighbors dies, as if by overpopulation.
+				if getneighbors(i, j) <= 4 {
+					grid[i][j] = 0
+				}
+
+				// Each cell with two or three neighbors survives
+				if getneighbors(i, j) == 2 || getneighbors(i, j) == 3 {
+					grid[i][j] = 1
+				}
+
+			} else if grid[i][j] == 0 {
+				// Each cell with three neighbors becomes populated.
+				if getneighbors(i, j) == 3 {
+					grid[i][j] = 1
+				}
+			}
+
+		}
+	}
+}
+
+func getneighbors(x, y int) int {
+	return grid[x][y+1] + grid[x][y-1] + grid[x-1][y] + grid[x+1][y] + grid[x-1][y-1] + grid[x-1][y+1] + grid[x+1][y-1] + grid[x+1][y-1]
+
+}
+
+// func checknegative(num int) int {
+// 	if num < 0 {
+// 		return 0
+// 	}
+// 	return num
+
+// }
 
 func display() {
 	time.Sleep(1 * time.Second)
@@ -28,7 +83,7 @@ func display() {
 	for i := 0; i < 20; i++ {
 		for j := 0; j < 20; j++ {
 			print("")
-			if gridstate[i][j] == 0 {
+			if grid[i][j] == 0 {
 				print("\033[37m ·")
 			} else {
 				print("\033[92m O")
@@ -40,34 +95,51 @@ func display() {
 	}
 }
 
-func setlife(life int) {
+func populate(life int) {
 
 	switch life {
 	case 0:
 		for i := 0; i < 20; i++ {
 			for j := 0; j < 20; j++ {
-				gridstate[i][j] = rand.Intn(2)
+				grid[i][j] = rand.Intn(2)
 			}
 		}
 	case 1:
-		gridstate[8][9] = 1
-		gridstate[9][9] = 1
-		gridstate[10][9] = 1
+		grid[8][9] = 1
+		grid[9][9] = 1
+		grid[10][9] = 1
 	case 2:
 		// row 1
-		gridstate[9][8] = 1
-		gridstate[9][9] = 1
-		gridstate[9][10] = 1
+		grid[9][8] = 1
+		grid[9][9] = 1
+		grid[9][10] = 1
 		// row 2
-		gridstate[8][9] = 1
-		gridstate[8][8] = 1
-		gridstate[8][7] = 1
+		grid[8][9] = 1
+		grid[8][8] = 1
+		grid[8][7] = 1
 	case 3:
-		gridstate[3][1] = 1
-		gridstate[3][2] = 1
-		gridstate[3][3] = 1
-		gridstate[2][3] = 1
-		gridstate[1][2] = 1
+		grid[3][1] = 1
+		grid[3][2] = 1
+		grid[3][3] = 1
+		grid[2][3] = 1
+		grid[1][2] = 1
 	}
 
 }
+
+/*
+	grid[9][10] = 1 // up
+	grid[9][8] = 1  //down
+
+	// left & right
+	grid[8][9] = 1
+	grid[10][9] = 1
+
+	// sad
+	grid[8][10] = 1
+	grid[8][8] = 1
+
+	grid[10][10] = 1
+	grid[10][8] = 1
+	grid[10]
+*/
